6b5d3c75ba5909bf7f1f17c71bbd0c677ae146e4
Mario Graf <mario.graf@bitmovin.net>
2015-03-30 21:14:05 +0200

    redesigned game states

--- src/game/state/GameRoundState.java
+++ src/game/state/GameRoundState.java
@@ -4,19 +4,17 @@ import game.Map;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * Created by Mario on 30.03.2015.
  */
-public class GameRoundState extends BasicGameState
+public class GameRoundState extends BombermanGameState
 {
-    private int id;
     private Map map = null;
 
-    public GameRoundState (int id) {
-        this.id = id;
+    public GameRoundState () {
+        super (BombermanGameState.GAME_ROUND);
     }
 
     @Override
@@ -33,10 +31,5 @@ public class GameRoundState extends BasicGameState
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
 
-    }
-
-    @Override
-    public int getID() {
-        return this.id;
     }
 }
--- src/game/state/MainMenuState.java
+++ src/game/state/MainMenuState.java
@@ -3,18 +3,15 @@ package game.state;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * Created by Mario on 30.03.2015.
  */
-public class MainMenuState extends BasicGameState
+public class MainMenuState extends BombermanGameState
 {
-    private int id;
-
-    public MainMenuState (int id) {
-        this.id = id;
+    public MainMenuState () {
+        super(BombermanGameState.MAIN_MENU);
     }
 
     @Override
@@ -30,10 +27,5 @@ public class MainMenuState extends BasicGameState
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
 
-    }
-
-    @Override
-    public int getID() {
-        return this.id;
     }
 }
