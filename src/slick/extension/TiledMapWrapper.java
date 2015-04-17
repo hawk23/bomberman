15b9cf004d0ace3b198b6fa25e6521faa39e942e
AenimaAut <albert.kraschl@gmx.at>
2015-04-17 17:39:26 +0200

    added TiledMapWrapper

---
+++ src/slick/extension/TiledMapWrapper.java
@@ -0,0 +1,12 @@
+package slick.extension;
+
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class TiledMapWrapper extends TiledMap {
+
+	public TiledMapWrapper(String ref) throws SlickException {
+		super(ref);
+	}
+
+}
