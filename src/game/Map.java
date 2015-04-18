af46c302e0838cd6404cd308a735ac8b50ddae17
rugbertl <rugbertl.gaming@gmx.at>
2015-04-18 19:25:52 +0200

    new 2 maps

--- src/game/Map.java
+++ src/game/Map.java
@@ -1,15 +1,11 @@
 package game;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.tiled.TiledMap;
-
-import slick.extension.AppGameContainerFSCustom;
 
 /**
  * Created by Mario on 30.03.2015.
