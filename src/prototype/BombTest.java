1ce37f097d2808d3260c614acebc340ad8a1cc2b
karatejack <vfx.schreier@gmail.com>
2015-04-18 12:07:00 +0200

    blub

--- src/prototype/BombTest.java
+++ src/prototype/BombTest.java
@@ -1,16 +1,10 @@
 package prototype;
 
-import game.model.Bomb;
-import game.model.OldBombermanMap;
-import game.model.Player;
+import game.debug.Debugger;
 import org.newdawn.slick.*;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.tiled.TiledMap;
+import slick.extension.ExplosionSystem;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,19 +16,9 @@ public class BombTest extends BasicGame {
     public static final int GAME_WIDTH = 1280;
     public static final int GAME_HEIGHT = 960;
     public static final String GAME_NAME = "BombTester";
-    public static final String MAP_PATH = "res/levels/0/map.tmx";
 
-    public static final int UPDATES_PER_SECOND = 40;
-    public static final int UPDATE_RATE = 1_000 / UPDATES_PER_SECOND;
-    public static final int TARGET_FPS = 500;
 
-    private OldBombermanMap map;
-    private int BLOCK_SIZE=64;
-
-    private List<Renderable> renderQue;
-    private Selector selector;
-
-    private String infoString="";
+    private ExplosionSystem explosionSystem;
 
     public BombTest(String title) {
         super(title);
@@ -53,45 +37,27 @@ public class BombTest extends BasicGame {
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        map.render(0,0,0,0,20,20,true);
-        for(Renderable renderable : renderQue){
-            renderable.draw(0,0);
-        }
-        graphics.drawString(infoString, 10, 25);
+        explosionSystem.render();
     }
 
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-        map.update(gameContainer,null,i);
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+        explosionSystem.update(delta);
     }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        map = new OldBombermanMap(MAP_PATH);
-        BLOCK_SIZE=map.getTileHeight();
-        selector = new Selector(BLOCK_SIZE);
-
-        renderQue=new ArrayList<Renderable>();
-        renderQue.add(selector);
+        explosionSystem = new ExplosionSystem();
     }
 
     @Override
     public void mouseMoved(int oldx, int oldy, int newx, int newy) {
-        Point point=map.pixelsToTile(newx,newy);
-
-        selector.setPosition(newx, newy);
-        infoString="("+newx+", "+newy+")=("+point.x+","+point.y+")";
     }
 
     @Override
-    public void mousePressed(int button, int x, int y){
-        Point point=map.pixelsToTile(x,y);
-        try {
-            map.addBomb(point.x, point.y, new Player(null, map, 0, null, null));
-        }catch(SlickException e){
-        }
-        //map.destroy(point.x,point.y);
-
+    public void mousePressed(int button, int posX, int posY){
+        Debugger.log("Added Explosion ("+posX+","+posY+")");
+        explosionSystem.addExplosion(posX,posY);
     }
 }
